06b5374647526e7dfaf6b286f8d8e023f6b04574
sudipta <basaks@gmail.com>
2016-12-06 18:53:41 +1100

    cleaned up optimiser example

--- pykrige/example/krige_optimiser.py
+++ pykrige/example/krige_optimiser.py
@@ -1,11 +1,12 @@
 import configparser
 import csv
 from collections import OrderedDict
+
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from pykrige.optimise import Krige
 
-from pykrige.optimise.krige import Krige
 config = configparser.ConfigParser()
 config.read_string(
     """
@@ -13,8 +14,6 @@ config.read_string(
     method = ordinary, universal
     variogram_model = linear, power, gaussian, spherical, exponential
     # insert other parameters
-    [output]
-    optimisation_output = optimisation.csv
     """
     )
 
@@ -46,19 +45,4 @@ y = 5*np.random.rand(100)
 
 # run the gridsearch
 estimator.fit(X=X, y=y)
-results = OrderedDict(estimator.cv_results_)
-
-# the output file from optimisation conf
-optimisation_output = config.get(section='output',
-                                 option='optimisation_output')
-out_keys = results.keys()
-
-with open(optimisation_output, 'w') as csv_file:
-    w = csv.DictWriter(csv_file, results.keys())
-    w.writeheader()
-    for r in zip(* results.values()):
-        w.writerow(
-            dict(zip(out_keys,
-                     [str(x) if not isinstance(x, str) else x for x in r]))
-        )
-
+print(OrderedDict(estimator.cv_results_))
