bfeb56a5f702fc2023e40b3038b1f787ee1cfc04
sudipta <basaks@gmail.com>
2016-12-27 19:52:20 +1100

    updated regression kriging example with sklearn.dataset california housing data

--- examples/regression_kriging.py
+++ examples/regression_kriging.py
@@ -1,32 +1,50 @@
 # coding: utf-8
-import numpy as np
-from itertools import product
 from pykrige.rk import RegressionKriging
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 
 svr_model = SVR(C=0.1)
 rf_model = RandomForestRegressor(n_estimators=100)
-lr_model = LinearRegression()
+lr_model = LinearRegression(normalize=True, copy_X=True, fit_intercept=False)
 
 models = [svr_model, rf_model, lr_model]
 
-# dummy data
+# take the first 5000 as Kriging is memory intensive
+housing = fetch_california_housing()
+X = housing['data'][:5000, :-2]
+lat_lon = housing['data'][:5000, -2:]
+target = housing['target'][:5000]
 
-np.random.seed(10)
-X = np.random.rand(100, 5)
-y = 2 + 10*X[:, 0] - 10*X[:, 1] - X[:, 2] + 3*X[:, 3] + 4*X[:, 4]
-lon = np.linspace(-180., 180.0, 20)
-lat = np.linspace(-90., 90., 20)
-np.random.shuffle(lon)
-np.random.shuffle(lat)
-
-lon_lat = np.array(list(product(lon[:10], lat[:10])))
+X_train, X_test, lat_lon_train, lat_lon_test, target_train, target_test \
+    = train_test_split(X, lat_lon, target, test_size=0.3, random_state=42)
 
 for m in models:
-    m_rk = RegressionKriging(ml_model=m, n_closest_points=15)
-    m_rk.fit(X[:70, :], lon_lat[:70, :], y[:70])
-    print('='*40, '\n', 'regression model:', m)
-    print(m_rk.predict(X[70:, :], lon_lat[70:, :]))
-    print('score: ', m_rk.score(X[70:, :], lon_lat[70:, :], y[70:]))
+    print('=' * 40, '\n', 'regression model:', m.__class__)
+    m_rk = RegressionKriging(ml_model=m, n_closest_points=10)
+    m_rk.fit(X_train, lat_lon_train, target_train)
+    print('Regression Score: ', m_rk.ml_model.score(X_test, target_test))
+    print('RK score: ', m_rk.score(X_test, lat_lon_test, target_test))
+
+##====================================OUTPUT==================================
+
+# ========================================
+#  regression model: <class 'sklearn.svm.classes.SVR'>
+# Finished learning regression model
+# Finished kriging residuals
+# Regression Score:  -0.034053855457
+# RK score:  0.66195576665
+# ========================================
+#  regression model: <class 'sklearn.ensemble.forest.RandomForestRegressor'>
+# Finished learning regression model
+# Finished kriging residuals
+# Regression Score:  0.699771164651
+# RK score:  0.737574040386
+# ========================================
+#  regression model: <class 'sklearn.linear_model.base.LinearRegression'>
+# Finished learning regression model
+# Finished kriging residuals
+# Regression Score:  0.527796839838
+# RK score:  0.604908933617
