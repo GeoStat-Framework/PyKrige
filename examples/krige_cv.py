a86c6091bd9664c27ab7b34d4138b7d85793474d
sudipta <basaks@gmail.com>
2017-02-02 18:48:56 +1100

    krige class now supports 3d kriging gridsearch

--- examples/krige_cv.py
+++ examples/krige_cv.py
@@ -3,6 +3,9 @@ import numpy as np
 from pykrige.rk import Krige
 from pykrige.compat import GridSearchCV
 
+
+# 2D Kring param opt
+
 param_dict = {"method": ["ordinary", "universal"],
               "variogram_model": ["linear", "power", "gaussian", "spherical"],
               # "nlags": [4, 6, 8],
@@ -17,6 +20,34 @@ y = 5 * np.random.rand(100)
 
 # run the gridsearch
 estimator.fit(X=X, y=y)
+
+
+if hasattr(estimator, 'best_score_'):
+    print('best_score R² = {:.3f}'.format(estimator.best_score_))
+    print('best_params = ', estimator.best_params_)
+
+print('\nCV results::')
+if hasattr(estimator, 'cv_results_'):
+    for key in ['mean_test_score', 'mean_train_score',
+                'param_method', 'param_variogram_model']:
+        print(' - {} : {}'.format(key, estimator.cv_results_[key]))
+
+# 3D Kring param opt
+
+param_dict3d = {"method": ["ordinary3d", "universal3d"],
+              "variogram_model": ["linear", "power", "gaussian", "spherical"],
+              # "nlags": [4, 6, 8],
+              # "weight": [True, False]
+              }
+
+estimator = GridSearchCV(Krige(), param_dict3d, verbose=True)
+
+# dummy data
+X3 = np.random.randint(0, 400, size=(100, 3)).astype(float)
+y = 5 * np.random.rand(100)
+
+# run the gridsearch
+estimator.fit(X=X3, y=y)
 
 
 if hasattr(estimator, 'best_score_'):
