09db696c91dc1bbd0a5b4cc5a9274523942f6d23
MuellerSeb <mueller.seb@posteo.de>
2020-03-26 13:05:30 +0100

    build doc against installed again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,24 +16,20 @@ import datetime
 import sys
 import os
 import shlex
-
 import matplotlib
-
 matplotlib.use("Agg")
 
 import sphinx_rtd_theme
-
 from recommonmark.parser import CommonMarkParser
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("../../"))
-import pykrige
+# sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("sphinxext"))
 
-
+import pykrige
 from github_link import make_linkcode_resolve
 
 source_parsers = {".md": CommonMarkParser}
