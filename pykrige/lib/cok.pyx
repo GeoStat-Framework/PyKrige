de40d3a9abe237b1e046c362dd53a5d974fc427d
MuellerSeb <mueller.seb@posteo.de>
2020-03-27 11:43:53 +0100

    cok: use scipy blas and lapack directly

--- pykrige/lib/cok.pyx
+++ pykrige/lib/cok.pyx
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 cimport numpy as np
-import scipy.linalg.blas
+# import scipy.linalg.blas
 from libc.math cimport sqrt
 import scipy.linalg
-from .lapack cimport dgemv, dgesv
+from scipy.linalg.cython_blas cimport dgemv
+from scipy.linalg.cython_lapack cimport dgesv
+# from .lapack cimport dgemv, dgesv
 from .variogram_models cimport get_variogram_model
 
 
