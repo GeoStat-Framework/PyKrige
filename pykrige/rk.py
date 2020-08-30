daa1d3d708dd085c69f6a5e47e503034b24f6430
mralbu <marceloralbuquerque@gmail.com>
2020-08-30 15:39:48 -0300

    Add ClassificationKriging

--- pykrige/rk.py
+++ pykrige/rk.py
@@ -1,14 +1,17 @@
 # coding: utf-8
+from scipy.linalg import helmert
+import numpy as np
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.svm import SVR
+from sklearn.base import RegressorMixin, ClassifierMixin, BaseEstimator
+from sklearn.preprocessing import OneHotEncoder
+from pykrige.uk3d import UniversalKriging3D
+from pykrige.ok3d import OrdinaryKriging3D
+from pykrige.uk import UniversalKriging
+from pykrige.ok import OrdinaryKriging
 from pykrige.compat import validate_sklearn
 
 validate_sklearn()
-from pykrige.ok import OrdinaryKriging
-from pykrige.uk import UniversalKriging
-from pykrige.ok3d import OrdinaryKriging3D
-from pykrige.uk3d import UniversalKriging3D
-from sklearn.base import RegressorMixin, BaseEstimator
-from sklearn.svm import SVR
-from sklearn.metrics import r2_score
 
 krige_methods = {
     "ordinary": OrdinaryKriging,
@@ -260,12 +263,18 @@ class Krige(RegressorMixin, BaseEstimator):
         return prediction, variance
 
 
-def check_sklearn_model(model):
+def check_sklearn_model(model, task="regression"):
     """Check the sklearn method in use."""
-    if not (isinstance(model, BaseEstimator) and isinstance(model, RegressorMixin)):
-        raise RuntimeError(
-            "Needs to supply an instance of a scikit-learn regression class."
-        )
+    if task == "regression":
+        if not (isinstance(model, BaseEstimator) and isinstance(model, RegressorMixin)):
+            raise RuntimeError(
+                "Needs to supply an instance of a scikit-learn regression class."
+            )
+    elif task == "classification":
+        if not (isinstance(model, BaseEstimator) and isinstance(model, ClassifierMixin)):
+            raise RuntimeError(
+                "Needs to supply an instance of a scikit-learn classification class."
+            )
 
 
 class RegressionKriging:
@@ -444,3 +453,294 @@ class RegressionKriging:
         return r2_score(
             y_pred=self.predict(p, x, **kwargs), y_true=y, sample_weight=sample_weight
         )
+
+
+class ClassificationKriging:
+    """
+    An implementation of Simplicial Indicator Kriging.
+
+    Parameters
+    ----------
+    classification_model: machine learning model instance from sklearn
+    method: str, optional
+        type of kriging to be performed
+    variogram_model: str, optional
+        variogram model to be used during Kriging
+    n_closest_points: int
+        number of closest points to be used during Ordinary Kriging
+    nlags: int
+        see OK/UK class description
+    weight: bool
+        see OK/UK class description
+    verbose: bool
+        see OK/UK class description
+    exact_values : bool
+        see OK/UK class description
+    variogram_parameters : list or dict
+        see OK/UK class description
+    variogram_function : callable
+        see OK/UK class description
+    anisotropy_scaling : tuple
+        single value for 2D (UK/OK) and two values in 3D (UK3D/OK3D)
+    anisotropy_angle : tuple
+        single value for 2D (UK/OK) and three values in 3D (UK3D/OK3D)
+    enable_statistics : bool
+        see OK class description
+    coordinates_type : str
+        see OK/UK class description
+    drift_terms : list of strings
+        see UK/UK3D class description
+    point_drift : array_like
+        see UK class description
+    ext_drift_grid : tuple
+        Holding the three values external_drift, external_drift_x and
+        external_drift_z for the UK class
+    functional_drift : list of callable
+        see UK/UK3D class description
+    """
+
+    def __init__(
+        self,
+        classification_model=SVR(),
+        method="ordinary",
+        variogram_model="linear",
+        n_closest_points=10,
+        nlags=6,
+        weight=False,
+        verbose=False,
+        exact_values=True,
+        pseudo_inv=False,
+        pseudo_inv_type="pinv",
+        variogram_parameters=None,
+        variogram_function=None,
+        anisotropy_scaling=(1.0, 1.0),
+        anisotropy_angle=(0.0, 0.0, 0.0),
+        enable_statistics=False,
+        coordinates_type="euclidean",
+        drift_terms=None,
+        point_drift=None,
+        ext_drift_grid=(None, None, None),
+        functional_drift=None,
+    ):
+        check_sklearn_model(classification_model, task="classification")
+        self.classification_model = classification_model
+        self.n_closest_points = n_closest_points
+        self._kriging_kwargs = dict(method=method,
+                                    variogram_model=variogram_model,
+                                    nlags=nlags,
+                                    weight=weight,
+                                    n_closest_points=n_closest_points,
+                                    verbose=verbose,
+                                    exact_values=exact_values,
+                                    pseudo_inv=pseudo_inv,
+                                    pseudo_inv_type=pseudo_inv_type,
+                                    variogram_parameters=variogram_parameters,
+                                    variogram_function=variogram_function,
+                                    anisotropy_scaling=anisotropy_scaling,
+                                    anisotropy_angle=anisotropy_angle,
+                                    enable_statistics=enable_statistics,
+                                    coordinates_type=coordinates_type,
+                                    drift_terms=drift_terms,
+                                    point_drift=point_drift,
+                                    ext_drift_grid=ext_drift_grid,
+                                    functional_drift=functional_drift)
+
+    def fit(self, p, x, y):
+        """
+        Fit the classification method and also krige the residual.
+
+        Parameters
+        ----------
+        p: ndarray
+            (Ns, d) array of predictor variables (Ns samples, d dimensions)
+            for classification
+        x: ndarray
+            ndarray of (x, y) points. Needs to be a (Ns, 2) array
+            corresponding to the lon/lat, for example 2d classification kriging.
+            array of Points, (x, y, z) pairs of shape (N, 3) for 3d kriging
+        y: ndarray
+            array of targets (Ns, )
+        """
+        self.classification_model.fit(p, y.ravel())
+        print("Finished learning classification model")
+        self.classes_ = self.classification_model.classes_
+
+        self.krige = []
+        for i in range(len(self.classes_) - 1):
+            self.krige.append(Krige(**self._kriging_kwargs))
+
+        ml_pred = self.classification_model.predict_proba(p)
+        ml_pred_ilr = ilr_transformation(ml_pred)
+
+        self.onehotencode = OneHotEncoder(
+            categories=[self.classes_]
+        )
+        y_ohe = np.array(self.onehotencode.fit_transform(y).todense())
+        y_ohe_ilr = ilr_transformation(y_ohe)
+
+        for i in range(len(self.classes_) - 1):
+            self.krige[i].fit(x=x, y=y_ohe_ilr[:, i] - ml_pred_ilr[:, i])
+
+        print("Finished kriging residuals")
+
+    def predict(self, p, x, **kwargs):
+        """
+        Predict.
+
+        Parameters
+        ----------
+        p: ndarray
+            (Ns, d) array of predictor variables (Ns samples, d dimensions)
+            for classification
+        x: ndarray
+            ndarray of (x, y) points. Needs to be a (Ns, 2) array
+            corresponding to the lon/lat, for example.
+            array of Points, (x, y, z) pairs of shape (N, 3) for 3d kriging
+
+        Returns
+        -------
+        pred: ndarray
+            The expected value of ys for the query inputs, of shape (Ns,).
+
+        """
+
+        ml_pred = self.classification_model.predict_proba(p)
+        ml_pred_ilr = ilr_transformation(ml_pred)
+
+        pred_proba_ilr = self.krige_residual(x, **kwargs) + ml_pred_ilr
+        pred_proba = inverse_ilr_transformation(pred_proba_ilr)
+
+        return np.argmax(pred_proba, axis=1)
+
+    def krige_residual(self, x, **kwargs):
+        """
+        Calculate the residuals.
+
+        Parameters
+        ----------
+        x: ndarray
+            ndarray of (x, y) points. Needs to be a (Ns, 2) array
+            corresponding to the lon/lat, for example.
+
+        Returns
+        -------
+        residual: ndarray
+            kriged residual values
+        """
+
+        krig_pred = [self.krige[i].predict(
+            x=x, **kwargs) for i in range(len(self.classes_) - 1)]
+
+        return np.vstack(krig_pred).T
+
+    def score(self, p, x, y, sample_weight=None, **kwargs):
+        """
+        Overloading default classification score method.
+
+        Parameters
+        ----------
+        p: ndarray
+            (Ns, d) array of predictor variables (Ns samples, d dimensions)
+            for classification
+        x: ndarray
+            ndarray of (x, y) points. Needs to be a (Ns, 2) array
+            corresponding to the lon/lat, for example.
+            array of Points, (x, y, z) pairs of shape (N, 3) for 3d kriging
+        y: ndarray
+            array of targets (Ns, )
+        """
+        return accuracy_score(
+            y_pred=self.predict(p, x, **kwargs), y_true=y, sample_weight=sample_weight
+        )
+
+
+def closure(data, k=1.0):
+    """Apply closure to data, sample-wise. 
+    Adapted from https://github.com/ofgulban/compoda.
+
+    Parameters
+    ----------
+    data : 2d numpy array, shape [n_samples, n_measurements]
+        Data to be closed to a certain constant. Do not forget to deal with
+        zeros in the data before this operation.
+    k : float, positive
+        Sum of the measurements will be equal to this number.
+
+    Returns
+    -------
+    data : 2d numpy array, shape [n_samples, n_measurements]
+        Closed data.
+
+    Reference
+    ---------
+    [1] Pawlowsky-Glahn, V., Egozcue, J. J., & Tolosana-Delgado, R.
+        (2015). Modelling and Analysis of Compositional Data, pg. 9.
+        Chichester, UK: John Wiley & Sons, Ltd.
+        DOI: 10.1002/9781119003144
+    """
+    data_sum = np.sum(data, axis=1)
+    out = np.copy(data)
+    for i in range(data.shape[1]):
+        out[:, i] = np.divide(out[:, i], data_sum)
+    out = out * k
+    return out
+
+
+def ilr_transformation(data):
+    """Isometric logratio transformation (not vectorized).
+    Adapted from https://github.com/ofgulban/compoda.
+
+    Parameters
+    ----------
+    data : 2d numpy array, shape [n_samples, n_coordinates]
+        Barycentric coordinates (closed) in simplex space.
+
+    Returns
+    -------
+    out : 2d numpy array, shape [n_samples, n_coordinates-1]
+        Coordinates in real space.
+
+    Reference
+    ---------
+    [1] Pawlowsky-Glahn, V., Egozcue, J. J., & Tolosana-Delgado, R.
+        (2015). Modelling and Analysis of Compositional Data, pg. 37.
+        Chichester, UK: John Wiley & Sons, Ltd.
+        DOI: 10.1002/9781119003144
+    """
+    data = data + np.finfo(float).eps
+    dims = data.shape
+    out = np.zeros((dims[0], dims[1]-1))
+    helmertian = helmert(dims[1]).T
+    for i in range(data.shape[0]):
+        out[i, :] = np.dot(np.log(data[i, :]), helmertian)
+    return out
+
+
+def inverse_ilr_transformation(data):
+    """Inverse isometric logratio transformation (not vectorized).
+    Adapted from https://github.com/ofgulban/compoda.
+
+    Parameters
+    ----------
+    data : 2d numpy array, shape [n_samples, n_coordinates]
+        Isometric log-ratio transformed coordinates in real space.
+
+    Returns
+    -------
+    out : 2d numpy array, shape [n_samples, n_coordinates+1]
+        Barycentric coordinates (closed) in simplex space.
+
+    Reference
+    ---------
+    [1] Pawlowsky-Glahn, V., Egozcue, J. J., & Tolosana-Delgado, R.
+        (2015). Modelling and Analysis of Compositional Data, pg. 37.
+        Chichester, UK: John Wiley & Sons, Ltd.
+        DOI: 10.1002/9781119003144
+    """
+    data = data + np.finfo(float).eps
+    dims = data.shape
+    out = np.zeros((dims[0], dims[1]+1))
+    helmertian = helmert(dims[1]+1)
+    for i in range(data.shape[0]):
+        out[i, :] = np.exp(np.dot(data[i, :], helmertian))
+    return closure(out)
---
+++ tests/test_classification_krige.py
@@ -0,0 +1,101 @@
+from itertools import product
+import pytest
+
+import numpy as np
+
+from pykrige.rk import ClassificationKriging
+
+try:
+    from sklearn.svm import SVC
+    from sklearn.datasets import fetch_california_housing
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.preprocessing import KBinsDiscretizer
+    from pykrige.compat import train_test_split
+
+    SKLEARN_INSTALLED = True
+except ImportError:
+    SKLEARN_INSTALLED = False
+
+
+def _methods():
+    krige_methods = ["ordinary", "universal"]
+    ml_methods = [
+        SVC(C=0.01, gamma="auto", probability=True),
+        RandomForestClassifier(n_estimators=50)
+    ]
+    return product(ml_methods, krige_methods)
+
+
+@pytest.mark.skipif(not SKLEARN_INSTALLED, reason="requires scikit-learn")
+def test_classification_krige():
+    np.random.seed(1)
+    x = np.linspace(-1.0, 1.0, 100)
+    # create a feature matrix with 5 features
+    X = np.tile(x, reps=(5, 1)).T
+    y = (
+        1
+        + 5 * X[:, 0]
+        - 2 * X[:, 1]
+        - 2 * X[:, 2]
+        + 3 * X[:, 3]
+        + 4 * X[:, 4]
+        + 2 * (np.random.rand(100) - 0.5)
+    )
+
+    # create lat/lon array
+    lon = np.linspace(-180.0, 180.0, 10)
+    lat = np.linspace(-90.0, 90.0, 10)
+    lon_lat = np.array(list(product(lon, lat)))
+
+    discretizer = KBinsDiscretizer(encode='ordinal')
+    y = discretizer.fit_transform(y.reshape(-1, 1))
+
+    X_train, X_test, y_train, y_test, lon_lat_train, lon_lat_test = train_test_split(
+        X, y, lon_lat, train_size=0.7, random_state=10
+    )
+
+    for ml_model, krige_method in _methods():
+        reg_class_model = ClassificationKriging(
+            classification_model=ml_model, method=krige_method, n_closest_points=2
+        )
+        reg_class_model.fit(X_train, lon_lat_train, y_train)
+        assert reg_class_model.score(X_test, lon_lat_test, y_test) > 0.25
+
+
+@pytest.mark.skipif(not SKLEARN_INSTALLED, reason="requires scikit-learn")
+def test_krige_classification_housing():
+    import ssl
+    import urllib
+
+    try:
+        housing = fetch_california_housing()
+    except (ssl.SSLError, urllib.error.URLError):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        try:
+            housing = fetch_california_housing()
+        except PermissionError:
+            # This can raise permission error on Appveyor
+            pytest.skip("Failed to load california housing dataset")
+        ssl._create_default_https_context = ssl.create_default_context
+
+    # take only first 1000
+    p = housing["data"][:1000, :-2]
+    x = housing["data"][:1000, -2:]
+    target = housing["target"][:1000]
+    discretizer = KBinsDiscretizer(encode='ordinal')
+    target = discretizer.fit_transform(target.reshape(-1, 1))
+
+    p_train, p_test, y_train, y_test, x_train, x_test = train_test_split(
+        p, target, x, train_size=0.7, random_state=10
+    )
+
+    for ml_model, krige_method in _methods():
+
+        reg_class_model = ClassificationKriging(
+            classification_model=ml_model, method=krige_method, n_closest_points=2
+        )
+        reg_class_model.fit(p_train, x_train, y_train)
+        if krige_method == "ordinary":
+            assert reg_class_model.score(p_test, x_test, y_test) > 0.5
+        else:
+            assert reg_class_model.score(p_test, x_test, y_test) > 0.0
