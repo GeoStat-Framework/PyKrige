060ced6afac66bba5f757abf670258f49e9ef051
yoel2000 <45918483+yoel2000@users.noreply.github.com>
2024-06-30 12:42:23 +0300

    contin ue

--- src/pykrige/ok.py
+++ src/pykrige/ok.py
@@ -274,7 +274,7 @@ class OrdinaryKriging:
         z = torch.tensor(z.values, dtype=torch.float32).to(device).squeeze()
         if z.dim() == 0:
             z = z.unsqueeze(0)
-        self.Z = np.atleast_1d(np.squeeze(np.array(z, copy=True, dtype=np.float32)))
+        # self.Z = np.atleast_1d(np.squeeze(np.array(z, copy=True, dtype=np.float32)))
 
         self.verbose = verbose
         self.enable_plotting = enable_plotting
